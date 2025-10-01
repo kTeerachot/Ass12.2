f36932d8e2eec55bdec02b305c0fef809b636b6c
kTeerachot <keng100847@gmail.com>
2025-10-01 21:26:52 +0700

    ESP32

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,9 @@ void setup() {
 void loop() {
     digitalWrite(LED_PIN, HIGH);
     Serial.println("LED ON");
-    delay(500);
+    delay(2000);
 
     digitalWrite(LED_PIN, LOW);
     Serial.println("LED OFF");
-    delay(500);
+    delay(2000);
 }
